3a240da62e3d696f25733252354abb89740e51b2
zsx0728 <zsx0728@163.com>
2021-02-03 10:55:37 +0800

    add btmp.h

---
+++ btmp.h
@@ -0,0 +1,33 @@
+
+#ifndef _PATH_BTMP
+# define _PATH_BTMP             "/var/log/btmp"
+#endif
+
+/* 根据starttime和endtime，从btmp日志中提取相应时间段的数据，并
+ * 将二进制数据写入新生成的文件
+ */
+void	generate_binlog(int32_t starttime, int32_t endtime)
+{
+	FILE    *fp_open_btmp, *fp_write_result;
+	struct  utmp buffer;
+
+	fp_open_btmp = fopen( _PATH_BTMP, "r");
+	fp_write_result = fopen( "./file.txt" , "w");
+
+	while (fread(&buffer, sizeof(buffer), 1, fp_open_btmp))
+	{
+    		if (buffer.ut_tv.tv_sec >= starttime && buffer.ut_tv.tv_sec <= endtime)
+    		{
+    			printf("%d	%d	%d	%s	%s\n", buffer.ut_tv.tv_sec, buffer.ut_type, 
+    				buffer.ut_addr_v6[4], buffer.ut_user, buffer.ut_host);
+			//大于starttime的条目以二进制形式写入文件
+			fwrite(&buffer, sizeof(buffer), 1, fp_write_result);
+
+    		}
+    		else
+    			continue;
+	}
+
+	fclose(fp_open_btmp);
+	fclose(fp_write_result);
+}
--- generate_logfile.c
+++ generate_logfile.c
@@ -1,5 +1,10 @@
+/* 如果需要分析的btmp文件路径不是/var/log/btmp，请按照如下格式自定义
+ * # define _PATH_BTMP             "/root/last_tmp/btmp"
+ */
+
 #include	<stdio.h>
 #include	<utmp.h>
+#include	"btmp.h"
 
 int main(void)
 {
@@ -7,29 +12,12 @@ int main(void)
 	struct	utmp buffer;
 	int32_t	starttime, endtime;
 	
-	printf("Please input the starttime:");
+	//输入提取数据的起始和终止时间，timestamp格式
+	printf("Please input the starttime(timestamp) :");
 	scanf("%d", &starttime);
-	printf("Please input the endtime:");
+	printf("Please input the endtime(timestamp) :");
 	scanf("%d", &endtime);
 
-	fp_open_btmp = fopen("/root/last_tmp/btmp", "r");
-	fp_write_result = fopen( "./file.txt" , "w");
-
-	printf("type	user	hostname	time	IP\n");
-	while (fread(&buffer, sizeof(buffer), 1, fp_open_btmp))
-	{
-    		if (buffer.ut_tv.tv_sec >= starttime && buffer.ut_tv.tv_sec <= endtime)
-    		{
-    			printf("%d	%d	%d	%s	%s\n", buffer.ut_tv.tv_sec, buffer.ut_type, 
-    				buffer.ut_addr_v6[4], buffer.ut_user, buffer.ut_host);
-			//大于starttime的条目以二进制形式写入文件
-			fwrite(&buffer, sizeof(buffer), 1, fp_write_result);
-
-    		}
-    		else
-    			continue;
-	}
-
-	fclose(fp_open_btmp);
-	fclose(fp_write_result);
+	//调用btmp.h内函数，打印时间段内日志数据并将二进制数据写入文件
+	generate_binlog(starttime, endtime);
 }
